fbeeadacafbd22da329c9d5772a785969dc10d4c
CoreTeam <core@smartrecruiters.com>
2020-09-28 09:23:20 +0200

    Brew formula update for rabbitr version 1.2.2

--- rabbitr.rb
+++ rabbitr.rb
@@ -2,16 +2,16 @@
 class Rabbitr < Formula
   desc "rabbitr - CLI tool for RabbitMQ management"
   homepage "https://github.com/smartrecruiters/rabbitr"
-  version "1.2.1"
+  version "1.2.2"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/smartrecruiters/rabbitr/releases/download/1.2.1/rabbitr_1.2.1_darwin_amd64.tar.gz"
-    sha256 "42cb3db192df5e6ab85af5fcfaedc6c737fd71214ccc8a672b6d99b5a912e60e"
+    url "https://github.com/smartrecruiters/rabbitr/releases/download/1.2.2/rabbitr_1.2.2_darwin_amd64.tar.gz"
+    sha256 "2e7e6a2ee1ed0efc53891a4dcefc7f23ae0c11967cf40b0f71bf184c1958e0db"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/smartrecruiters/rabbitr/releases/download/1.2.1/rabbitr_1.2.1_linux_amd64.tar.gz"
-      sha256 "049bb5ed35a148141674528e4d638c7d903b8410d853e75a005ed2e07427a56a"
+      url "https://github.com/smartrecruiters/rabbitr/releases/download/1.2.2/rabbitr_1.2.2_linux_amd64.tar.gz"
+      sha256 "27015d8946e78eb01b5d31a574be63450f59ca9eb4e4e4bfda454337c3b45034"
     end
   end
 
